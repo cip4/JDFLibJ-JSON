124bc621270ef4e3fdd2a722f98dbfa91cd49b7d
Stefan Meissner <stefan.meissner@vistaprint.com>
2021-01-24 20:14:33 +0100

    Initial Commit

---
+++ src/main/java/org/cip4/jdflib/json/Main.java
@@ -0,0 +1,15 @@
+package org.cip4.jdflib.json;
+
+
+import org.cip4.jdflib.core.JDFVersion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+	public String getJdfLibVersion() {
+		return JDFVersion.LIB_MAJOR_VERSION;
+	}
+}
---
+++ src/test/java/org/cip4/jdflib/json/MainTests.java
@@ -0,0 +1,24 @@
+package org.cip4.jdflib.json;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTests {
+
+	Main main = new Main();
+
+	@Test
+	void getJdfLibVersion() {
+
+		// arrange
+
+		// act
+		String result = main.getJdfLibVersion();
+
+		// assert
+		Assertions.assertEquals("2.1.7", result, "Version is wrong.");
+
+	}
+
+}
